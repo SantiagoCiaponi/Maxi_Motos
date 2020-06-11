0738a74625e9a403f92e42e74c828bb6986d4f39
Santiago Ciaponi <SantiagoCiaponi3@gmail.com>
2020-06-11 11:13:39 -0300

    v beta. Agregado de todas las motos usadas.

--- scripts/scripts_usado.js
+++ scripts/scripts_usado.js
@@ -64,11 +64,56 @@ const ltz = new Motos(
   `${url}/motos_usadas/quadsport3.jpg`,
   `El Suzuki LTZ 400 cuenta con un motor monocilindrico de 500 cm3, 4 tiempos enfriado por aire, OHC 1 árbol de levas, y 17 CV de potencia.
    La transmisión es de 5 velocidades mas la marcha atras.`,
-  `S u peso es de 183kg. El freno delantero es de doble disco de 160 mm. El trasero de tambor.
+  `Su peso es de 183kg. El freno delantero es de doble disco de 160 mm. El trasero de tambor.
    La suspensión delantera es Independiente, doble horquilla, amortiguador hidráulico con resortes, la trasera, Basculante con amortiguador sencillo`,
   `La posición de manejo invita a pasar largas horas a bordo de la AS.
   La AS ofrece una postura relajada, con los codos levemente flexionados, la espalda erguida y una flexión de piernas confortable.`,
   "botonltz"
+);
+
+const cbx = new Motos(
+  "Honda CBX250 Twister",
+  `${url}/motos_usadas/hondacbx1.jpg`,
+  `${url}/motos_usadas/hondacbx2.jpg`,
+  `${url}/motos_usadas/hondacbx3.jpg`,
+  `La Honda CBX250 Twister cuenta con un motor Monocilindrico 4 tiempos de 249 cm3. Su sistema de arranque es electrico y 
+   su potencia máxima es de 23.66 HP a 8000 rpm. Su transmisión es de 6 velocidades.`,
+  `La horquilla telescópica delantera sirve como estructura del chasis del vehículo y como medio para mover el vehículo.
+   La suspensión trasera es mono-amortiguada.  El amortiguador está ubicado debajo del asiento y está dispuesto de tal 
+   manera que la llanta trasera se encuentre siempre en contacto con el suelo.`,
+  `Carburador tipo CV con el cual se permite que el funcionamiento del cable del acelerador actúe en la válvula de 
+  aceleración tipo mariposa, separadamente del venturí.`,
+  "botoncbx"
+);
+
+const cryptonusada = new Motos(
+  "Yamaha Crypton",
+  `${url}/motos_usadas/crypton1.jpg`,
+  `${url}/motos_usadas/crypton2.jpg`,
+  `${url}/motos_usadas/crypton3.jpg`,
+  `Con un andar cómodo y sencillo, es una excelente opción para la movilidad urbana diaria. Posee un motor 4 tiempos de
+   100 cm3 que llega a una potencia máxima de 8 hp a 7.500 rpm, y una velocidad máxima da 80 km/h.`,
+  `Por su estética y dimensiones es un vehículo versátil y fácil de conducir en trayectos cortos y medianos.
+   Además posee un completo tablero, caballete central y lateral así como baúl portaobjetos bajo el asiento.`,
+  `El arranque de esta moto es electrico y a pedal, cuenta con caja de 4 velocidades y la capacidad del tanque es de 
+   5,2 Lts. Es una moto muy económica, y por eso es una muy buena opción para una moto urbana.`,
+  "botoncrypton"
+);
+
+const cb = new Motos(
+  "Honda CB1 ",
+  `${url}/motos_usadas/hondacb1.jpg`,
+  `${url}/motos_usadas/hondacb2.jpg`,
+  `${url}/motos_usadas/hondacb3.jpg`,
+  `La CB1 es una moto que ante todo es una herramienta de trabajo. 
+  Por esto su diseño es básico y a la vez completamente dirigido a la funcionalidad. 
+  Cuenta con una estructura fuerte y resistente que permite un manejo cómodo dirigido a la ergonomía.`,
+  `La CB1 es una moto sin sofisticaciones, pero con fortaleza y características mecánicas para trabajar.
+   El equipo de Honda la probó arduamente en todo el territorio colombiano antes de su lanzamiento para 
+   asegurar que cumple con las expectativas de los consumidores a quienes se dirige.`,
+  `Cuenta con un motor cuatro tiempos de 124,8 cc, la cb1 entrega 8.5 HP a 7.500rpm.
+  E s de encendido por patada y cuenta con frenos de tambor en las dos ruedas.`,
+  "botoncb"
 );
 
 // Función poner motos
@@ -125,6 +170,18 @@ $botonrouser.addEventListener("click", () => showModal(rouser));
 //-----Suzuki LTZ 400
 const $botonltz = document.querySelector(".botonltz");
 $botonltz.addEventListener("click", () => showModal(ltz));
+
+//-----Honda CBX250 Twister
+const $botoncbx = document.querySelector(".botoncbx");
+$botoncbx.addEventListener("click", () => showModal(cbx));
+
+//-----Yamaha Crypton
+const $botoncrypton = document.querySelector(".botoncrypton");
+$botoncrypton.addEventListener("click", () => showModal(cryptonusada));
+
+//-----Honda CB1
+const $botoncb = document.querySelector(".botoncb");
+$botoncb.addEventListener("click", () => showModal(cb));
 
 //--------------------------------------------------------
 
